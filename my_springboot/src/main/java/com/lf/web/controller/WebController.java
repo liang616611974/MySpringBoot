098b290eb29a3d0b0f2afa01634e2338f1b9addf
liangfeng <liangfeng@healthmall.cn>
2017-04-26 10:06:28 +0800

    第三次提交

---
+++ my_springboot/src/main/java/com/lf/web/controller/WebController.java
@@ -0,0 +1,30 @@
+/**
+ * 2015-2016 龙果学院 (www.roncoo.com)
+ */
+package com.lf.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * spring-boot-demo-6-2
+ * 
+ * @author wujing
+ */
+@Controller
+@RequestMapping(value = "/web")
+public class WebController {
+
+	private static final Logger logger = LoggerFactory.getLogger(WebController.class);
+	@RequestMapping(value = "index")
+	public String index(ModelMap map) {
+		logger.debug("debug hahah================");
+		logger.info("info ====================");
+		map.put("title", "thymeleaf hello word");
+		return "index";
+	}
+
+}
