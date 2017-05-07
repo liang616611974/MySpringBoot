f7055d15676480e020912688c8fa68c0b7cf5599
liangfeng <616611974@qq.com>
2017-05-08 07:33:43 +0800

    修改了MySpringBootApp启动类的类路径

---
+++ myspringboot-web-admin/src/main/java/com/liangfeng/myspringboot/MySpringBootApp.java
@@ -0,0 +1,30 @@
+package com.liangfeng.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+
+/**
+ * @Title: MySpringBootApp.java
+ * @Description: 系统启动入口
+ * @author Liangfeng
+ * @date 2017/4/28 0028 下午 3:54
+ * @version 1.0
+ */
+@SpringBootApplication
+public class MySpringBootApp {
+
+    @Bean
+    public Object testBean(PlatformTransactionManager platformTransactionManager){
+        System.out.println(">>>>>>>>>>" + platformTransactionManager.getClass().getName());
+        return new Object();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApp.class, args);
+    }
+
+
+}
